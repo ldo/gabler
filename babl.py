7f34a4ffea0cb453cc1e7f9ae90125d36d6b460b
Lawrence D'Oliveiro <ldo@geek-central.gen.nz>
2022-05-31 04:45:17 +0000

    Create high-level Babl wrapper object with some initial methods

--- babl.py
+++ babl.py
@@ -10,18 +10,18 @@ wrapper to work.
 #-
 
 import ctypes as ct
-
-# from babl-0.1/babl/babl-types.h:
-
-BablPtr = ct.c_void_p
+from weakref import \
+    WeakValueDictionary
 
 class BABL :
     "useful constants and types from include files."
 
     # from babl-0.1/babl/babl-types.h:
 
-    FuncLinear = ct.CFUNCTYPE(None, BablPtr, ct.c_char_p, ct.c_char_p, ct.c_long, ct.c_void_p)
-    FuncPlanar = ct.CFUNCTYPE(None, BablPtr, ct.c_int, ct.POINTER(ct.c_char_p), ct.POINTER(ct.c_int), ct.c_int, ct.POINTER(ct.c_char_p), ct.POINTER(ct.c_int), ct.c_long, ct.c_void_p)
+    Ptr = ct.c_void_p
+
+    FuncLinear = ct.CFUNCTYPE(None, Ptr, ct.c_char_p, ct.c_char_p, ct.c_long, ct.c_void_p)
+    FuncPlanar = ct.CFUNCTYPE(None, Ptr, ct.c_int, ct.POINTER(ct.c_char_p), ct.POINTER(ct.c_int), ct.c_int, ct.POINTER(ct.c_char_p), ct.POINTER(ct.c_int), ct.c_long, ct.c_void_p)
 
     # from babl-0.1/babl/babl.h:
 
@@ -53,7 +53,7 @@ class BABL :
     SPACE_FLAG_NONE = 0
     SPACE_FLAG_EQUALIZE = 1
 
-    FishProcess = ct.CFUNCTYPE(None, BablPtr, ct.c_char_p, ct.c_char_p, ct.c_long, ct.c_void_p)
+    FishProcess = ct.CFUNCTYPE(None, Ptr, ct.c_char_p, ct.c_char_p, ct.c_long, ct.c_void_p)
 
     ALPHA_FLOOR = 1 / 65536
 
@@ -70,102 +70,190 @@ libbabl.babl_init.restype = None
 libbabl.babl_exit.argtypes = ()
 libbabl.babl_exit.restype = None
 libbabl.babl_type.argtypes = (ct.c_char_p,)
-libbabl.babl_type.restype = BablPtr
+libbabl.babl_type.restype = BABL.Ptr
 libbabl.babl_sampling.argtypes = (ct.c_int, ct.c_int)
-libbabl.babl_sampling.restype = BablPtr
+libbabl.babl_sampling.restype = BABL.Ptr
 libbabl.babl_component.argtypes = (ct.c_char_p,)
-libbabl.babl_component.restype = BablPtr
+libbabl.babl_component.restype = BABL.Ptr
 libbabl.babl_model.argtypes = (ct.c_char_p,)
-libbabl.babl_model.restype = BablPtr
-libbabl.babl_model_with_space.argtypes = (ct.c_char_p, BablPtr)
-libbabl.babl_model_with_space.restype = BablPtr
+libbabl.babl_model.restype = BABL.Ptr
+libbabl.babl_model_with_space.argtypes = (ct.c_char_p, BABL.Ptr)
+libbabl.babl_model_with_space.restype = BABL.Ptr
 libbabl.babl_space.argtypes = (ct.c_char_p,)
-libbabl.babl_space.restype = BablPtr
+libbabl.babl_space.restype = BABL.Ptr
 libbabl.babl_space_from_icc.argtypes = (ct.c_void_p, ct.c_int, BABL.IccIntent, ct.POINTER(ct.c_char_p))
-libbabl.babl_space_from_icc.restype = BablPtr
-libbabl.babl_space_get_gamma.argtypes = (BablPtr,)
+libbabl.babl_space_from_icc.restype = BABL.Ptr
+libbabl.babl_space_get_gamma.argtypes = (BABL.Ptr,)
 libbabl.babl_space_get_gamma.restype = ct.c_double
 # babl_icc_make_space deprecated
 libbabl.babl_icc_get_key.argtypes = (ct.c_void_p, ct.c_int, ct.c_char_p, ct.c_char_p, ct.c_char_p)
 libbabl.babl_icc_get_key.restype = ct.c_char_p
 libbabl.babl_format.argtypes = (ct.c_char_p,)
-libbabl.babl_format.restype = BablPtr
-libbabl.babl_format_with_space.argtypes = (ct.c_char_p, BablPtr)
-libbabl.babl_format_with_space.restype = BablPtr
+libbabl.babl_format.restype = BABL.Ptr
+libbabl.babl_format_with_space.argtypes = (ct.c_char_p, BABL.Ptr)
+libbabl.babl_format_with_space.restype = BABL.Ptr
 libbabl.babl_format_exists.argtypes = (ct.c_char_p,)
 libbabl.babl_format_exists.restype = ct.c_bool
-libbabl.babl_format_get_space.argtypes = (BablPtr,)
-libbabl.babl_format_get_space.restype = BablPtr
+libbabl.babl_format_get_space.argtypes = (BABL.Ptr,)
+libbabl.babl_format_get_space.restype = BABL.Ptr
 libbabl.babl_fish.argtypes = (ct.c_void_p, ct.c_void_p)
-libbabl.babl_fish.restype = BablPtr
+libbabl.babl_fish.restype = BABL.Ptr
 libbabl.babl_fast_fish.argtypes = (ct.c_void_p, ct.c_void_p, ct.c_char_p)
-libbabl.babl_fast_fish.restype = BablPtr
-libbabl.babl_process.argtypes = (BablPtr, ct.c_void_p, ct.c_void_p, ct.c_long)
+libbabl.babl_fast_fish.restype = BABL.Ptr
+libbabl.babl_process.argtypes = (BABL.Ptr, ct.c_void_p, ct.c_void_p, ct.c_long)
 libbabl.babl_process.restype = ct.c_long
-libbabl.babl_process_rows.argtypes = (BablPtr, ct.c_void_p, ct.c_int, ct.c_void_p, ct.c_int, ct.c_long, ct.c_int)
+libbabl.babl_process_rows.argtypes = (BABL.Ptr, ct.c_void_p, ct.c_int, ct.c_void_p, ct.c_int, ct.c_long, ct.c_int)
 libbabl.babl_process_rows.restype = ct.c_long
-libbabl.babl_get_name.argtypes = (BablPtr,)
+libbabl.babl_get_name.argtypes = (BABL.Ptr,)
 libbabl.babl_get_name.restype = ct.c_char_p
-libbabl.babl_format_has_alpha.argtypes = (BablPtr,)
+libbabl.babl_format_has_alpha.argtypes = (BABL.Ptr,)
 libbabl.babl_format_has_alpha.restype = ct.c_bool
-libbabl.babl_format_get_bytes_per_pixel.argtypes = (BablPtr,)
+libbabl.babl_format_get_bytes_per_pixel.argtypes = (BABL.Ptr,)
 libbabl.babl_format_get_bytes_per_pixel.restype = ct.c_int
-libbabl.babl_format_get_model.argtypes = (BablPtr,)
-libbabl.babl_format_get_model.restype = BablPtr
-libbabl.babl_get_model_flags.argtypes = (BablPtr,)
+libbabl.babl_format_get_model.argtypes = (BABL.Ptr,)
+libbabl.babl_format_get_model.restype = BABL.Ptr
+libbabl.babl_get_model_flags.argtypes = (BABL.Ptr,)
 libbabl.babl_get_model_flags.restype = BABL.ModelFlag
-libbabl.babl_format_get_n_components.argtypes = (BablPtr,)
+libbabl.babl_format_get_n_components.argtypes = (BABL.Ptr,)
 libbabl.babl_format_get_n_components.restype = ct.c_int
-libbabl.babl_format_get_type.argtypes = (BablPtr, ct.c_int)
-libbabl.babl_format_get_type.restype = BablPtr
+libbabl.babl_format_get_type.argtypes = (BABL.Ptr, ct.c_int)
+libbabl.babl_format_get_type.restype = BABL.Ptr
 # babl_model_new, babl_format_new -- use varargs, can’t access
-libbabl.babl_format_n.argtypes = (BablPtr, ct.c_int)
-libbabl.babl_format_n.restype = BablPtr
-libbabl.babl_format_is_format_n.argtypes = (BablPtr,)
+libbabl.babl_format_n.argtypes = (BABL.Ptr, ct.c_int)
+libbabl.babl_format_n.restype = BABL.Ptr
+libbabl.babl_format_is_format_n.argtypes = (BABL.Ptr,)
 libbabl.babl_format_is_format_n.restype = ct.c_bool
 # babl_conversion_new -- uses varargs, can’t access
-libbabl.babl_conversion_get_source_space.argtypes = (BablPtr,)
-libbabl.babl_conversion_get_source_space.restype = BablPtr
-libbabl.babl_conversion_get_destination_space.argtypes = (BablPtr,)
-libbabl.babl_conversion_get_destination_space.restype = BablPtr
-libbabl.babl_new_palette.argtypes = (ct.c_char_p, ct.POINTER(BablPtr), ct.POINTER(BablPtr))
-libbabl.babl_new_palette.restype = BablPtr
-libbabl.babl_new_palette_with_space.argtypes = (ct.c_char_p, BablPtr, ct.POINTER(BablPtr), ct.POINTER(BablPtr))
-libbabl.babl_new_palette_with_space.restype = BablPtr
-libbabl.babl_format_is_palette.argtypes = (BablPtr,)
+libbabl.babl_conversion_get_source_space.argtypes = (BABL.Ptr,)
+libbabl.babl_conversion_get_source_space.restype = BABL.Ptr
+libbabl.babl_conversion_get_destination_space.argtypes = (BABL.Ptr,)
+libbabl.babl_conversion_get_destination_space.restype = BABL.Ptr
+libbabl.babl_new_palette.argtypes = (ct.c_char_p, ct.POINTER(BABL.Ptr), ct.POINTER(BABL.Ptr))
+libbabl.babl_new_palette.restype = BABL.Ptr
+libbabl.babl_new_palette_with_space.argtypes = (ct.c_char_p, BABL.Ptr, ct.POINTER(BABL.Ptr), ct.POINTER(BABL.Ptr))
+libbabl.babl_new_palette_with_space.restype = BABL.Ptr
+libbabl.babl_format_is_palette.argtypes = (BABL.Ptr,)
 libbabl.babl_format_is_palette.restype = ct.c_bool
-libbabl.babl_palette_set_palette.argtypes = (BablPtr, BablPtr, ct.c_void_p, ct.c_int)
+libbabl.babl_palette_set_palette.argtypes = (BABL.Ptr, BABL.Ptr, ct.c_void_p, ct.c_int)
 libbabl.babl_palette_set_palette.restype = None
-libbabl.babl_palette_reset.argtypes = (BablPtr,)
+libbabl.babl_palette_reset.argtypes = (BABL.Ptr,)
 libbabl.babl_palette_reset.restype = None
-libbabl.babl_set_user_data.argtypes = (BablPtr, ct.c_void_p)
+libbabl.babl_set_user_data.argtypes = (BABL.Ptr, ct.c_void_p)
 libbabl.babl_set_user_data.restype = None
-libbabl.babl_get_user_data.argtypes = (BablPtr,)
+libbabl.babl_get_user_data.argtypes = (BABL.Ptr,)
 libbabl.babl_get_user_data.restype = ct.c_void_p
-libbabl.babl_space_from_chromaticities.argtypes = (ct.c_char_p, ct.c_double, ct.c_double, ct.c_double, ct.c_double, BablPtr, BablPtr, BablPtr, BABL.SpaceFlags)
-libbabl.babl_space_from_chromaticities.restype = BablPtr
+libbabl.babl_space_from_chromaticities.argtypes = (ct.c_char_p, ct.c_double, ct.c_double, ct.c_double, ct.c_double, BABL.Ptr, BABL.Ptr, BABL.Ptr, BABL.SpaceFlags)
+libbabl.babl_space_from_chromaticities.restype = BABL.Ptr
 libbabl.babl_trc_gamma.argtypes = (ct.c_double,)
-libbabl.babl_trc_gamma.restype = BablPtr
+libbabl.babl_trc_gamma.restype = BABL.Ptr
 libbabl.babl_trc.argtypes = (ct.c_char_p,)
-libbabl.babl_trc.restype = BablPtr
-libbabl.babl_space_with_trc.argtypes = (BablPtr, BablPtr)
-libbabl.babl_space_with_trc.restype = BablPtr
-libbabl.babl_space_get.argtypes = (BablPtr, ct.POINTER(ct.c_double), ct.POINTER(ct.c_double), ct.POINTER(ct.c_double), ct.POINTER(ct.c_double), ct.POINTER(ct.c_double), ct.POINTER(ct.c_double), ct.POINTER(ct.c_double), ct.POINTER(ct.c_double), ct.POINTER(BablPtr), ct.POINTER(BablPtr), ct.POINTER(BablPtr))
+libbabl.babl_trc.restype = BABL.Ptr
+libbabl.babl_space_with_trc.argtypes = (BABL.Ptr, BABL.Ptr)
+libbabl.babl_space_with_trc.restype = BABL.Ptr
+libbabl.babl_space_get.argtypes = (BABL.Ptr, ct.POINTER(ct.c_double), ct.POINTER(ct.c_double), ct.POINTER(ct.c_double), ct.POINTER(ct.c_double), ct.POINTER(ct.c_double), ct.POINTER(ct.c_double), ct.POINTER(ct.c_double), ct.POINTER(ct.c_double), ct.POINTER(BABL.Ptr), ct.POINTER(BABL.Ptr), ct.POINTER(BABL.Ptr))
 libbabl.babl_space_get.restype = None
-libbabl.babl_space_get_rgb_luminance.argtype = (BablPtr, ct.POINTER(ct.c_double), ct.POINTER(ct.c_double), ct.POINTER(ct.c_double))
+libbabl.babl_space_get_rgb_luminance.argtype = (BABL.Ptr, ct.POINTER(ct.c_double), ct.POINTER(ct.c_double), ct.POINTER(ct.c_double))
 libbabl.babl_space_get_rgb_luminance.restype = None
-libbabl.babl_model_is.argtypes = (BablPtr, ct.c_char_p)
+libbabl.babl_model_is.argtypes = (BABL.Ptr, ct.c_char_p)
 libbabl.babl_model_is.restype = ct.c_bool
   # overridden by babl_model_is macro which calls babl_model_with_space instead
-libbabl.babl_space_get_icc.argtypes = (BablPtr, ct.POINTER(ct.c_int))
+libbabl.babl_space_get_icc.argtypes = (BABL.Ptr, ct.POINTER(ct.c_int))
 libbabl.babl_space_get_icc.restype = ct.c_char_p
-libbabl.babl_space_from_rgbxyz_matrix.argtypes = (ct.c_char_p, ct.c_double, ct.c_double, ct.c_double, ct.c_double, ct.c_double, ct.c_double, ct.c_double, ct.c_double, ct.c_double, ct.c_double, ct.c_double, ct.c_double, BablPtr, BablPtr, BablPtr)
-libbabl.babl_space_from_rgbxyz_matrix.restype = BablPtr
-libbabl.babl_format_get_encoding.argtypes = (BablPtr,)
+libbabl.babl_space_from_rgbxyz_matrix.argtypes = (ct.c_char_p, ct.c_double, ct.c_double, ct.c_double, ct.c_double, ct.c_double, ct.c_double, ct.c_double, ct.c_double, ct.c_double, ct.c_double, ct.c_double, ct.c_double, BABL.Ptr, BABL.Ptr, BABL.Ptr)
+libbabl.babl_space_from_rgbxyz_matrix.restype = BABL.Ptr
+libbabl.babl_format_get_encoding.argtypes = (BABL.Ptr,)
 libbabl.babl_format_get_encoding = ct.c_char_p
-libbabl.babl_space_is_cmyk.argtypes = (BablPtr,)
+libbabl.babl_space_is_cmyk.argtypes = (BABL.Ptr,)
 libbabl.babl_space_is_cmyk.restype = ct.c_bool
-libbabl.babl_space_is_gray.argtypes = (BablPtr,)
+libbabl.babl_space_is_gray.argtypes = (BABL.Ptr,)
 libbabl.babl_space_is_gray.restype = ct.c_bool
-libbabl.babl_fish_get_process.argtypes = (BablPtr,)
+libbabl.babl_fish_get_process.argtypes = (BABL.Ptr,)
 libbabl.babl_fish_get_process.restype = BABL.FishProcess
+
+#+
+# Higher-level stuff
+#-
+
+class Babl :
+
+    __slots__ = ("_bablobj", "__weakref__")
+
+    _instances = WeakValueDictionary()
+
+    def __new__(celf, _bablobj) :
+        self = celf._instances.get(_bablobj)
+        if self == None :
+            self = super().__new__(celf)
+            self._bablobj = _bablobj
+            celf._instances[_bablobj] = self
+        #end if
+        return \
+            self
+    #end __new__
+
+    # no need for __del__ -- babl manages its own storage?
+
+    @classmethod
+    def format(celf, encoding) :
+        return \
+            celf(libbabl.babl_format(encoding.encode()))
+    #end format
+
+    @classmethod
+    def format_with_space(celf, encoding, space) :
+        if not isinstance(space, celf) :
+            raise TypeError("space must be a %s" % celf.__name__)
+        #end if
+        return \
+            celf(libbabl.babl_format_with_space(encoding.encode()), space._bablobj)
+    #end format
+
+    @property
+    def name(self) :
+        return \
+            libbabl.babl_get_name(self._bablobj).decode()
+    #end name
+
+    @property
+    def bytes_per_pixel(self) :
+        return \
+            libbabl.babl_format_get_bytes_per_pixel(self._bablobj)
+    #end bytes_per_pixel
+
+    @property
+    def model(self) :
+        return \
+            type(self)(libbabl.babl_format_get_model(self._bablobj))
+    #end model
+
+    @property
+    def model_flags(self) :
+        return \
+            libbabl.babl_get_model_flags(self._bablobj)
+    #end model_flags
+
+    @property
+    def n_components(self) :
+        return \
+            libbabl.babl_format_get_n_components(self._bablobj)
+    #end n_components
+
+    def get_type(self, component_index) :
+        return \
+            type(self)(libbabl.babl_format_get_type(self._bablobj, component_index))
+    #end get_type
+
+    def __repr__(self) :
+        return \
+            "<Babl %s>" % repr(self.name)
+    #end __repr__
+
+#end Babl
+
+def init() :
+    libbabl.babl_init()
+#end init
+
+def exit() :
+    libbabl.babl_exit()
+#end exit
